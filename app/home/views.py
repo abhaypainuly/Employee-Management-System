04ddeb753778d6af36ef9d63061a4974ef158e3c
abhaypainuly <painuly.abhay@gmail.com>
2021-11-23 16:34:20 +0530

    Adding /user blueprint

---
+++ app/home/views.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from flask_login import login_required
+
+from . import home
+
+@home.route("/")
+def homepage():
+    """
+    Renders the homepage template!
+    """
+    return render_template("home/index.html", title="Welcome")
+
+@home.route("/dashboard")
+@login_required
+def dashboard():
+    """
+    Renders the dashboard template!
+    """
+    return render_template("home/dabhboard.html", title="Dashboard")
