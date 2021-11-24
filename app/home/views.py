3c142e6032b1bf3be4906adf20cb32050ffa0230
abhaypainuly <painuly.abhay@gmail.com>
2021-11-24 15:19:41 +0530

    Adding /home/admin_dashboard.html, changes in home/views.py and some changes in NAv bar---> base.html

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,5 @@
-from flask import render_template, flash
-from flask_login import login_required
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_required, current_user
 
 from . import home
 
@@ -17,3 +17,12 @@ def dashboard():
     Renders the dashboard template!
     """
     return render_template("home/dashboard.html", title="Dashboard")
+
+@home.route("/admin/dashboard")
+@login_required
+def admin_dashboard():
+    if not current_user.is_admin:
+        # Redirecting to normal dashboard for non admin users
+        return redirect(url_for("home.dashboard"))
+
+    return render_template("home/admin_dashboard.html", title="Admin Dashboard")
